6699e88ef84b38a84707c4456d192f1e4dab5c9a
Samiul Anwar <samiul.anwar@gmail.com>
2016-06-30 18:29:15 -0400

    sub function working as intended

--- methods.js
+++ methods.js
@@ -2,14 +2,19 @@
 
 function sumFinder(arr,target){
 	var retArr=[];
+	var indexes=[];
 	for(var i=0;i<arr.length;i++){
 		for(var m=i+1;m<arr.length;m++){
-			if(arr[i]+arr[m]===target){
+			if((arr[i]+arr[m]===target)&&(indexes.includes(i)===false)&&(indexes.includes(m)===false)){
 				retArr.push(arr[i],arr[m]);
+				indexes.push(i,m);
 			}
 		}
 	}
-	return retArr;
+	//return retArr;
+	return indexes;
 }
 
-sumFinder([1,4,2,3,0,5],7);
+//sumFinder([1,4,2,3,0,5],7);
+//sumFinder([0, 0, 0, 0, 1, 1], 1); //0,4,1,5,
+//sumFinder([1, 1, 1], 2);
